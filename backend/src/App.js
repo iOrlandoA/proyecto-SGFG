e4cbb6ceac9da2be33a71f728970517b8b27ba28
alearal12 <alejandro.araya@ucrso.info>
2023-05-25 12:37:13 -0600

    api test

--- backend/src/App.js
+++ backend/src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+
+  const getData = async(url) => {
+    const newData = await fetch(url, {
+      method: 'GET',
+      headers: {
+        'content-type': 'application/json',
+        'Accept': 'application/json'
+      }
+      })
+      .then(res => res.json());
+      console.log(newData);
+  }
+
+  getData('/api');
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
     </div>
   );
 }
